895a1111a08c18e432564e677dd95e189a9a3cc8
DuffRumkins <shamzo.mcginley@gmail.com>
2021-03-22 22:07:56 +0100

    Added a first version of left/right detection and confidence level

--- sw/airborne/modules/computer_vision/ground_detection_testing.c
+++ sw/airborne/modules/computer_vision/ground_detection_testing.c
@@ -29,16 +29,30 @@ uint8_t cr_max  = 0;
 //TODO: make either boolean or integer with less bits
 volatile int go_no_go;
 
+//Integer that will be negative if obstacle on the right and negative if obstacle on the right
+volatile int left_or_right;
+
+//double <1 that is higher the clearer the drones path is
+volatile double confidence_level;
+
 //Remember image is rotated. Width is longer of the two dimensions
 //Length of the rectangle most to the left as percentage of image length (height)
 double BOTTOM_LENGTH_PERCENTAGE = 0.5;
 //Length of the rectangle most to the right as percentage of image length (height)
 double TOP_LENGTH_PERCENTAGE = 0.25;
+//How far to the left the rectangles start as a percentage of image width
+//TODO: add to dlsettings
+double BOTTOM_WIDTH_PERCENTAGE = 0;
 //How far to the left the rectangles extend as a percentage of image width
 double TOP_WIDTH_PERCENTAGE = 0.75;
 //#width in pixels of each rectangle
 int WIDTH_RECT = 5;
-
+//length of each square (the squares are the rectangles in each column. They do not have to be squares)
+//TODO: add to dlsettings
+int LENGTH_SQUARE = 5;
+//Every SQUARES_CHECKEDth square is checked for greer
+//TODO: add to dl settings
+int SQUARES_CHECKED = 2;
 
 int check_for_green(struct image_t *img, int right_corner_row, int right_corner_column, int rect_length) {
     //printf("Working\n");
@@ -103,39 +117,75 @@ struct image_t *get_rect(struct image_t *img){ //In this function we want to loo
 
     //The number of rectangles which are considered. It is set so that the space
     //up to TOP_WIDTH_PERCENTAGE is filled with non-overlapping rectangles
-    int num_rect = ceil(TOP_WIDTH_PERCENTAGE*columns/WIDTH_RECT);
-    //printf("num_rect is equal to %d\n",num_rect);
+    int num_rect = ceil((TOP_WIDTH_PERCENTAGE-BOTTOM_WIDTH_PERCENTAGE)*columns/WIDTH_RECT);
+
     //The difference in length between 2 adjacent rectangles in percentage of image length (height)
     double rect_length_increment = (BOTTOM_LENGTH_PERCENTAGE-TOP_LENGTH_PERCENTAGE)/num_rect;
-    //printf("rect_length_increment is equal to %lf \n",rect_length_increment);
+
+    //initialise only_green_in_row which will be 1 if a row has no green and 0 otherwise
+    int only_green_in_row;
+
     //Go through rectangles starting from smallest (most to the right)
     for (int rect_num = 0; rect_num < num_rect; rect_num++) {
 
         //The length of the rectangle for this iteration pixels
         int rect_length = floor((TOP_LENGTH_PERCENTAGE + rect_num * rect_length_increment)*rows);
-        //Coordinates of right corner
-        int right_corner_row = 0.5*(rows-rect_length);
-        int right_corner_column = TOP_WIDTH_PERCENTAGE*columns - rect_num*WIDTH_RECT;
 
-        //go_no_go = check_for_green(img, right_corner_row, right_corner_column, rect_length);
-        //printf("Rectangle at (%d,%d) of length (%d) is a %d \n", right_corner_column, right_corner_row, rect_length,go_no_go);
+        //This is the number of squares in each row
+        int num_squares = ceil(rect_length/LENGTH_SQUARE);
 
-        //Check if this rectangle is completely green and if so we are good to go straight ahead
-        if (check_for_green(img, right_corner_row, right_corner_column, rect_length) == 1) {
-            //printf("Rectangle at (%d,%d) of length (%d) is a go \n", right_corner_column, right_corner_row, rect_length);
-            go_no_go = 1;
-            return img;
+        for (int square_num = 0; square_num < num_squares; square_num++) {
+
+            //initialise no_green_in_row which will be 1 if a row has no g
+            only_green_in_row = 1;
+
+            //Coordinates of right corner
+            int right_corner_row = 0.5 * ((rows - rect_length) + LENGTH_SQUARE*square_num);
+            int right_corner_column = TOP_WIDTH_PERCENTAGE * columns - WIDTH_RECT * rect_num;
+
+            //go_no_go = check_for_green(img, right_corner_row, right_corner_column, rect_length);
+            //printf("Rectangle at (%d,%d) of length (%d) is a %d \n", right_corner_column, right_corner_row, rect_length,go_no_go);
+
+            if (square_num%SQUARES_CHECKED == 0){
+                //TODO: add else to add point to left right score if square comes back as not green
+                //Check if this rectangle is completely green and if so we are good to go straight ahead
+                printf("square number %d at row %d and column %d) is being checked \n", square_num, right_corner_row, right_corner_column);
+                if (check_for_green(img, right_corner_row, right_corner_column, rect_length) == 0) {
+                    //printf("Rectangle at (%d,%d) of length (%d) is a go \n", right_corner_column, right_corner_row, rect_length);
+
+                    //Reset only_green_in_row for each new rectangle
+                    only_green_in_row = 0;
+
+                    //TODO:add more in depth weights depending on distance of square from center line
+                    //if square is to the left of the center of the image subtract 1 from left_or_right
+                    if (right_corner_row+0.5*LENGTH_SQUARE < 0.5*rows){
+                        left_or_right--;
+                    }
+                    //else add 1 to left_or_right
+                    else{
+                        left_or_right++;
+                    }
+
+                    printf("The left_or_right score is %d \n", left_or_right);
+                }
+            }
+            //if a rectangle has no green
+            if (only_green_in_row == 1) {
+                //set confidence level to decrease the closer the only green rectangle to the drone
+                confidence_level = (num_rect-rect_num)/num_rect;
+                go_no_go = 1;
+                printf("We are good to go with a confidence level of %lf",confidence_level);
+                return img;
+            }
         }
     }
 
+    confidence_level = 0.0;
     go_no_go = 0;
     return img;
 
     //go_no_go = check_for_green(img,PERCENT_WIDTH_RECT_1,PERCENT_HEIGHT_RECT_1);
     //printf("To go or not to go: %d \n",go_no_go);
-
-
-    return img;
 }
 
 void image_width_printer_init(void) {
